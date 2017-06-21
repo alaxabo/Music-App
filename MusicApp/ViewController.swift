48273b352bd670589be82bc8ecdebc676fd36a50
alaxabo <trantuananh2008@gmail.com>
2017-06-21 08:57:07 +0700

    Initial Commit

---
+++ MusicApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by Alaxabo on 6/21/17.
+//  Copyright © 2017 Alaxabo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
